b839026c13bcd1e8240e8b528ceb25a22788511a
david-ist <73030764+david-ist@users.noreply.github.com>
2020-11-21 10:16:49 +0100

    Update queries.py

--- queries.py
+++ queries.py
@@ -8,46 +8,47 @@ dbconfig = {"host": "XXXXXX",
 
 #Select statement to retrieve website URL
 def select(site, st):
-  try:
-      with UseDatabase(dbconfig) as cursor: 
-          _SQL_select = """select WEBSITE_URL from website.site where website_url = %s"""
-           cursor.execute(_SQL_select,(site,))
-           data = cursor.fetchall()
-           print(st + " Select executed")
-           return data
-   except ConnectionError as err:
+    try:
+        with UseDatabase(dbconfig) as cursor: 
+            _SQL_select = """select WEBSITE_URL from website.site where website_url = %s"""
+            cursor.execute(_SQL_select,(site,))
+            data = cursor.fetchall()
+            print(st + " Select executed")
+            return data
+    except ConnectionError as err:
         print(err)
-   except CredentialsError as err:
+    except CredentialsError as err:
         print(err)
-   except Exception as err:
+    except Exception as err:
         print(err)
 
 #Insert statement 
 def insert(site, title, body_nospace, body_raw, st):
-  try:
-      with UseDatabase(dbconfig) as cursor: 
-          _SQL_insert = """INSERT INTO website.site (WEBSITE_URL, WEBSITE_TITLE, SITE_BODY, SITE_HTML) VALUES (%s, %s, %s, %s)"""
-          cursor.execute(_SQL_insert,(str(site), str(title), str(body_nospace), str(body_raw)))
-          print(st + " INSERT executed")
-   except ConnectionError as err:
+    try:
+        with UseDatabase(dbconfig) as cursor: 
+            _SQL_insert = """INSERT INTO website.site (WEBSITE_URL, WEBSITE_TITLE, SITE_BODY, SITE_HTML) VALUES (%s, %s, %s, %s)"""
+            cursor.execute(_SQL_insert,(str(site), str(title), str(body_nospace), str(body_raw)))
+            print(st + " INSERT executed")
+    except ConnectionError as err:
         print(err)
-   except CredentialsError as err:
+    except CredentialsError as err:
         print(err)
-   except Exception as err:
+    except Exception as err:
         print(err)
 
 #Update the database
 def update(site, title, body_nospace, body_raw, st):
-  try:
+    try:
        with UseDatabase(dbconfig) as cursor: 
             _SQL_update = """UPDATE website.site SET WEBSITE_TITLE = %s, SITE_BODY = %s, SITE_HTML = %s where WEBSITE_URL = %s"""
-             cursor.execute(_SQL_update,(str(title), str(body_nospace), str(body_raw), site))
-             print(st + " UPDATE executed")
-   except ConnectionError as err:
+            cursor.execute(_SQL_update,(str(title), str(body_nospace), str(body_raw), site))
+            print(st + " UPDATE executed")
+    except ConnectionError as err:
         print(err)
-   except CredentialsError as err:
+    except CredentialsError as err:
         print(err)
-   except Exception as err:
+    except Exception as err:
+       print(err)
 
 #Simple select for data lookup
 def lookup_select():
